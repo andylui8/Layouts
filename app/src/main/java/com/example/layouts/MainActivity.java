cfd6185253acc5872edee971c75ebdc26ee54a94
andylui8 <andyluilin@gmail.com>
2020-11-17 22:31:47 +0100

    actualizado

--- app/src/main/java/com/example/layouts/MainActivity.java
+++ app/src/main/java/com/example/layouts/MainActivity.java
@@ -9,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear_layout);
+        //setContentView(R.layout.linear_layout);
+        //setContentView(R.layout.table_layout);
+        //setContentView(R.layout.relative_layout);
+        setContentView(R.layout.constraint_layout);
     }
 }
